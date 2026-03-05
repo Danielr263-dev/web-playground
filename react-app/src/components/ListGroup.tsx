aed913d86f9510dd568cf0a594b1d20b3b83dabc
Danielr263-dev <danielri263@yahoo.com>
2026-03-04 23:00:42 -0500

    Updates to ListGroup.tsx

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,12 +1,26 @@
 function ListGroup() {
+    let items = [
+        'New york',
+        'paris',
+        'london',
+        'tokyo',
+        'mexico city'
+    ]
+
     return (
-        <ul className="list-group">
-            <li className="list-group-item">An item</li>
-            <li className="list-group-item">A second item</li>
-            <li className="list-group-item">A third item</li>
-            <li className="list-group-item">A fourth item</li>
-            <li className="list-group-item">And a fifth one</li>
-        </ul>
+        //<> wraps everything in it as a fragment automatically
+        //items.map is like an array called items, arrow transforms it to list array that displays all items ind
+        //key adds a key to each individual item so that adding/removing things dynamically is better 
+        //to do anything dynamically, {} are needed
+        //when doing: condition && item, result is = to item if true, null if false
+        <>
+            <h1> List</h1>
+            {items.length === 0 && <p>No item found</p>}
+            <ul className="list-group">
+             {items.map((item, index) => <li className = "list-group-item" key = {item} onClick={(event) => console.log(event)} >{item}</li>)}
+             
+            </ul>
+        </>
     );
 }   
 
