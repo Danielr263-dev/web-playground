2347b433ea154326a48f8455db2fbe604a989a63
Danielr263-dev <danielri263@yahoo.com>
2026-03-19 19:28:36 -0400

    updates

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,7 +1,18 @@
 import ListGroup from "./components/ListGroup";
 
 function App(){
-  return <div> <ListGroup/> </div>
+  let items = [
+        'New york',
+        'paris',
+        'london',
+        'tokyo',
+        'mexico city'
+    ]
+    const handleSelectItem = (item: String) => {
+      console.log(item);
+    }
+
+  return <div> <ListGroup items = {items} heading = "Cities" onSelectItem={handleSelectItem}/> </div>
 }
 
 export default App;
--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,15 +1,13 @@
-//import { MouseEvent } from "react";
+interface Props {
+    items: string[];
+    heading: string;
+    onSelectItem: (item: String) => void;
+}
 
 import { useState } from "react";
 
-function ListGroup() {
-    let items = [
-        'New york',
-        'paris',
-        'london',
-        'tokyo',
-        'mexico city'
-    ]
+function ListGroup({items, heading, onSelectItem}: Props) {
+
     //hook, states to react that we'll have data that changes overtime
     const [selectedIndex, setSelectedIndex] = useState(-1);
    
@@ -25,10 +23,10 @@ function ListGroup() {
         //when doing: condition && item, result is = to item if true, null if false
         //event: MouseEvent is a type annotation to describe the type, event is. 
         <>
-            <h1> List</h1>
+            <h1>{heading}</h1>
             {items.length === 0 && <p>No item found</p>}
             <ul className="list-group">
-             {items.map((item, index) => <li className = {selectedIndex === index ? 'list-group-item active' : 'list-group-item'} key = {item} onClick={() => {setSelectedIndex(index); } } >{item}</li>)}
+             {items.map((item, index) => <li className = {selectedIndex === index ? 'list-group-item active' : 'list-group-item'} key = {item} onClick={() => {setSelectedIndex(index), onSelectItem(item); } } >{item}</li>)}
              
             </ul>
         </>
